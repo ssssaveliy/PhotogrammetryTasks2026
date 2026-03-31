8fd405e7bfb6374571ea75db995f5be91a1d81c4
Boris <simiyutin.boris@yandex.ru>
2026-03-31 14:24:08 +0300

    pin eigen version to 3.4 for macos to fix ceres cmake build
    Replaced Homebrew installation with manual download and build process for Eigen3.

--- .github/scripts/macos/install_eigen3.sh
+++ .github/scripts/macos/install_eigen3.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+set -euo pipefail
 
-brew install eigen
+wget https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz
+tar xzf eigen-3.4.0.tar.gz
+cd eigen-3.4.0
+cmake -B build -DCMAKE_INSTALL_PREFIX=/opt/eigen3
+sudo cmake --build build --target install
