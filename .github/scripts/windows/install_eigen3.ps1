765e552f3d736d75f4ac3f52ad8bb237441ed8c0
Boris <simiyutin.boris@yandex.ru>
2026-03-31 20:14:53 +0300

    pin eigen3 version for Windows
    Added script to download and install Eigen3 using CMake.

--- .github/scripts/windows/install_eigen3.ps1
+++ .github/scripts/windows/install_eigen3.ps1
@@ -1,1 +1,10 @@
-vcpkg install eigen3
+$ErrorActionPreference = "Stop"
+$INSTALL_PREFIX = "C:\eigen-3.4.0"
+
+Invoke-WebRequest -Uri "https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.zip" -OutFile "eigen-3.4.0.zip"
+Expand-Archive -Path "eigen-3.4.0.zip" -DestinationPath "."
+cd eigen-3.4.0
+cmake -S . -B _build -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX"
+if ($LASTEXITCODE -ne 0) { throw "cmake configure failed" }
+cmake --install _build
+if ($LASTEXITCODE -ne 0) { throw "cmake install failed" }
