1ef5ff1ddf5b2f8b598644ac9391b853a9b0812b
Boris <simiyutin.boris@yandex.ru>
2026-03-31 14:09:11 +0300

    fixed missing cd for macos ceres build

--- .github/scripts/macos/install_ceres_solver.sh
+++ .github/scripts/macos/install_ceres_solver.sh
@@ -7,7 +7,7 @@ wget https://github.com/ceres-solver/ceres-solver/archive/2.2.0.zip
 unzip 2.2.0.zip
 cd ceres-solver-2.2.0
 rm -rf build
-mkdir build
+mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=/opt/ceres-solver220 -DUSE_CUDA=OFF ..
 make -j$(sysctl -n hw.ncpu)
 sudo make install
